9acb49346a3291ea48a22595a68cd092ff958a5c
ajinkyathorat5 <ajinkya.thorat@volkswagen.co.in>
2022-02-23 15:36:03 +0530

    testing demo rest app for db connection

---
+++ src/main/java/com/example/test/TestApplication.java
@@ -0,0 +1,29 @@
+package com.example.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/test")
+public class TestApplication {
+	@Autowired
+	PartBlacklistRespository partBlacklistRespository;
+	@GetMapping
+	public List<PartBlacklist> testIt(){
+		System.out.println("PArts black list size:-----"+ partBlacklistRespository.findAll().size());
+		return partBlacklistRespository.findAll();
+
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+
+	}
+
+}
