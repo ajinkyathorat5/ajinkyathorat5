93f9611f2c1476c238894dd262692a8aafd794d8
ajinkyathorat5 <88368930+ajinkyathorat5@users.noreply.github.com>
2022-02-24 10:49:53 +0530

    Delete src directory

--- src/main/java/com/example/test/PartBlacklistRespository.java
+++
@@ -1,6 +0,0 @@
-package com.example.test;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PartBlacklistRespository extends JpaRepository <PartBlacklist, Integer>{
-}
